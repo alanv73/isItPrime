2344f07ac754561ea960e18d40ed199073d72c7f
Alan Van Art <alanv73@gmail.com>
2019-12-12 13:19:14 -0500

    Initial Commit

---
+++ isItPrime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  isItPrime
+//
+//  Created by Alan Van Art on 12/12/19.
+//  Copyright © 2019 Alan Van Art. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
