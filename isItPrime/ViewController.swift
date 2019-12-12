e70e7e4c49499da53bce9dbe0d236ac62ad67c50
Alan Van Art <alanv73@gmail.com>
2019-12-12 13:53:53 -0500

    initial commit

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    @IBOutlet weak var txtInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblOutput.text = ""
     }
 
-
-}
+    @IBAction func checkClick(_ sender: Any) {
+        var numToCheck:Int?
+        
+        numToCheck = Int(txtInput.text ?? "")
+        
+        if isPrime(n: numToCheck!) {
+            lblOutput.text = "Yes"
+        } else {
+            lblOutput.text = "No"
+        }
+    }
+    
+    func isPrime(n: Int) -> Bool {
+        if n < 2 {
+            return false // are not primes
+        }
+        
+        let limit = Int(sqrt(Float(n)))
+        
+        if limit < 2 {
+           return true // 2, 3 are primes
+        }
+        
+        for i in 2...limit {
+            if n % i == 0 {
+                return false
+            }
+        }
+        
+        return true
+    }}
 
